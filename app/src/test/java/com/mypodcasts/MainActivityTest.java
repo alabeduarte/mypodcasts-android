c8fa8f5ffc2854c891c5792d2545bbf1877689c5
Alabê Duarte <alabeduarte@gmail.com>
2015-06-23 20:42:52 -0300

    Add ROME to fetch RSS

--- app/src/test/java/com/mypodcasts/MainActivityTest.java
+++ app/src/test/java/com/mypodcasts/MainActivityTest.java
@@ -1,17 +1,22 @@
 package com.mypodcasts;
 
+import android.app.Activity;
+
 import com.mypodcasts.support.CustomTestRunner;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 import static org.robolectric.Robolectric.buildActivity;
 
 @RunWith(CustomTestRunner.class)
 public class MainActivityTest {
+
   @Test
   public void testSomething() {
-    assertTrue(buildActivity(MainActivity.class).create().get() != null);
+    Activity activity = buildActivity(MainActivity.class).create().get();
+
+    assertNotNull(activity);
   }
 }
