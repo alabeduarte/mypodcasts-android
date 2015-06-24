60b1d6ef9d1dbc40d3bc4ca866a6fe0a3262d874
Alabê Duarte <alabeduarte@gmail.com>
2015-06-24 18:49:38 -0300

    Introduce Roboguice

--- app/src/main/java/com/mypodcasts/MainActivity.java
+++ app/src/main/java/com/mypodcasts/MainActivity.java
@@ -5,18 +5,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.mypodcasts.rss.Feed;
 
-public class MainActivity extends Activity {
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends RoboActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.menu_main, menu);
     return true;
   }
--- app/src/test/java/com/mypodcasts/MainActivityTest.java
+++ app/src/test/java/com/mypodcasts/MainActivityTest.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 
 import com.mypodcasts.support.CustomTestRunner;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -13,10 +14,16 @@ import static org.robolectric.Robolectric.buildActivity;
 @RunWith(CustomTestRunner.class)
 public class MainActivityTest {
 
-  @Test
-  public void testSomething() {
-    Activity activity = buildActivity(MainActivity.class).create().get();
+  private MainActivity activity;
 
-    assertNotNull(activity);
+  @Before
+  public void setup() {
+    activity = buildActivity(MainActivity.class).create().get();
+  }
+
+  @Test
+  public void itLoadsLatestEpisodeGivenUrl() {
+    activity.onCreate(null);
+
   }
 }
