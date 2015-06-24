178662b5b189a55806870160b57c142eadceb8a7
Alabê Duarte <alabeduarte@gmail.com>
2015-06-24 20:29:12 -0300

    Add empty list view

--- app/src/main/java/com/mypodcasts/MainActivity.java
+++ app/src/main/java/com/mypodcasts/MainActivity.java
@@ -1,21 +1,50 @@
 package com.mypodcasts;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.mypodcasts.rss.Feed;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 
 @ContentView(R.layout.activity_main)
 public class MainActivity extends RoboActivity {
 
+  @InjectView(R.id.episodesListView)
+  ListView episodesListView;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    episodesListView.setAdapter(new BaseAdapter() {
+      @Override
+      public int getCount() {
+        return 1;
+      }
+
+      @Override
+      public Object getItem(int position) {
+        return null;
+      }
+
+      @Override
+      public long getItemId(int position) {
+        return 0;
+      }
+
+      @Override
+      public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = getLayoutInflater();
+        return inflater.inflate(R.layout.episode_item, parent, false);
+      }
+    });
   }
 
   @Override
--- app/src/test/java/com/mypodcasts/MainActivityTest.java
+++ app/src/test/java/com/mypodcasts/MainActivityTest.java
@@ -1,6 +1,8 @@
 package com.mypodcasts;
 
-import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.mypodcasts.support.CustomTestRunner;
 
@@ -8,22 +10,41 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.robolectric.Robolectric.buildActivity;
 
 @RunWith(CustomTestRunner.class)
 public class MainActivityTest {
 
-  private MainActivity activity;
+  MainActivity activity;
+  ListView listView;
 
   @Before
   public void setup() {
     activity = buildActivity(MainActivity.class).create().get();
+    activity.onCreate(null);
+
+    listView = (ListView) activity.findViewById(R.id.episodesListView);
   }
 
   @Test
-  public void itLoadsLatestEpisodeGivenUrl() {
-    activity.onCreate(null);
+  public void itReturnsEpisodesCount() {
+    assertThat(listView.getAdapter().getCount(), is(1));
+  }
 
+  @Test
+  public void itInflatesEachRow() {
+    int position = 0;
+    View convertView = new View(activity);
+    ViewGroup parent = new ViewGroup(activity) {
+      @Override
+      protected void onLayout(boolean changed, int l, int t, int r, int b) {
+      }
+    };
+
+    View row = listView.getAdapter().getView(position, convertView, parent);
+
+    assertThat(row.getVisibility(), is(View.VISIBLE));
   }
 }
