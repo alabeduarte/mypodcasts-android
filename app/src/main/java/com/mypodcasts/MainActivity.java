ccc5a14704c90e6c0775a69661d51f2433d35b78
Alabê Duarte <alabeduarte@gmail.com>
2015-06-25 12:36:45 -0300

    Remove unused actionbar

--- app/src/main/java/com/mypodcasts/MainActivity.java
+++ app/src/main/java/com/mypodcasts/MainActivity.java
@@ -1,8 +1,6 @@
 package com.mypodcasts;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.mypodcasts.podcasts.EpisodesAdapter;
@@ -25,26 +23,5 @@ public class MainActivity extends RoboActivity {
     super.onCreate(savedInstanceState);
 
     episodesListView.setAdapter(new EpisodesAdapter(new ArrayList<Episode>(), getLayoutInflater()));
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
   }
 }
