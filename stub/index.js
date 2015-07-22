2a07bf43e86ffddf34bf57bf9d1bf845ada2696a
Alabê Duarte <alabeduarte@gmail.com>
2015-07-21 23:28:48 -0300

    Improve stub to return user's feeds

--- stub/index.js
+++ stub/index.js
@@ -3,15 +3,28 @@ var app = express();
 
 var fs = require("fs");
 
+var fromFile = function(fileName) {
+  return fs.readFileSync(__dirname + fileName, 'utf-8');
+};
+
+var jsonParse = function(file) {
+  return JSON.parse(file);
+};
+
+var simulateHighLatency = function(response, json) {
+  setTimeout(function() { response.json(json) }, 5000);
+};
+
 app.get('/api/user/johndoe/latest_episodes', function(req, res) {
-  var file = fs.readFileSync(__dirname + '/latest_episodes.json', 'utf-8');
-  var json = JSON.parse(file);
+  var json = jsonParse(fromFile('/latest_episodes.json'));
 
-  var start = Date.now();
+  simulateHighLatency(res, json);
+});
 
-  setTimeout(function() {
-    res.json(json)
-  }, 5000);
+app.get('/api/user/johndoe/feeds', function(req, res) {
+  var json = jsonParse(fromFile('/user_feeds.json'));
+
+  simulateHighLatency(res, json);
 });
 
 var server = app.listen(3000, function () {
