7d1692922cc164ef266b57b57ecd3fefa67363e6
Alabê Duarte <alabeduarte@gmail.com>
2015-07-25 10:49:46 -0300

    Add scripts for build pipeline

---
+++ ci/release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Upload app to TestFairy"
+
+rm app-release-unsigned.apk
+cp app/build/outputs/apk/app-release-unsigned.apk .
+
+curl https://app.testfairy.com/api/upload -F api_key=$API_KEY -F file=@app-release-unsigned.apk
