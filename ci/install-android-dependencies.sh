f850da94e8cc3f301b060114f6469b8c405c513c
Fernando <fernandoasfj@gmail.com>
2015-07-27 10:20:11 -0300

    CI script to install android and required packages

---
+++ ci/install-android-dependencies.sh
@@ -0,0 +1,6 @@
+download-android
+echo y | android update sdk --no-ui --filter platform-tool
+echo y | android update sdk --no-ui --filter android-19
+echo y | android update sdk --no-ui --filter android-21
+echo y | android update sdk --no-ui --all --filter build-tools-21.1.2 --force
+echo y | android update sdk --no-ui --filter extra
