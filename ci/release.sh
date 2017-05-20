c3f01fdb5633d4f9d1dc6b0f6458e6253584ec84
Alabê Duarte <alabeduarte@gmail.com>
2017-05-20 17:26:07 +1000

    Clean up after release

--- ci/release.sh
+++ ci/release.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 ./gradlew assemble
+
 echo "Upload app to TestFairy"
 
 rm app-release-unsigned.apk
 cp app/build/outputs/apk/app-release-unsigned.apk .
 
 curl https://app.testfairy.com/api/upload -F api_key=$API_KEY -F file=@app-release-unsigned.apk
+
+./gradlew clean
